06b948014a282a7c5b0d8c6f03fce825c25998d3
Kerita <1332924387@qq.com>
2017-05-16 18:36:18 +0800

    reg,login,logout

---
+++ README.md
@@ -0,0 +1,3 @@
+# 使用 Express + MongoDB 搭建多人博客
+
+参考链接:[http://wiki.jikexueyuan.com/project/express-mongodb-setup-blog/](http://wiki.jikexueyuan.com/project/express-mongodb-setup-blog/)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+	devtool: '#source-map',
+	entry: [
+		'./bin/www',
+		'webpack/hot/poll?1000'
+	],
+	output: {
+		path: path.join(__dirname, "out"),
+		libraryTarget: 'commonjs2',
+		filename: "bundle.js"
+	},
+	module: {
+    	loaders: [
+    		{test: /\.js$/, loaders: ['express-hot-reload', 'babel-loader'], exclude: /node_modules/}
+    	]
+
+  	},
+	externals: /^[a-z][a-z\.\-0-9]*$/,
+	target: "node",
+	plugins:[new webpack.NoErrorsPlugin()]
+};
